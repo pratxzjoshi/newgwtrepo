e15b33a8c4afad02e2e02336a3740077665cb0b0
pratxzjoshi <131383624+pratxzjoshi@users.noreply.github.com>
2023-04-25 11:10:13 +0530

    Create App.java

---
+++ .github/App.java
@@ -0,0 +1,39 @@
+package com.app;
+
+ 
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+
+
+ 
+
+/**
+* Unit test for simple App.
+*/
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+ 
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+ 
+
+   
