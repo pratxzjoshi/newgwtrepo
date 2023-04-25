efc918b01f52b7a7f7df1ef91603825794ffeb74
pratxzjoshi <131383624+pratxzjoshi@users.noreply.github.com>
2023-04-25 11:15:19 +0530

    Update myfile.java

--- .github/myfile.java
+++ .github/myfile.java
@@ -4,16 +4,16 @@ public class Main {
   public static void main(String[] args) {
     ArrayList<String> fruits = new ArrayList<String>();
 
-    // Add some elements to the ArrayList
+    
     fruits.add("Apple");
     fruits.add("Banana");
     fruits.add("Mango");
     fruits.add("Pineapple");
 
-    // Retrieve an element from the ArrayList using get() method
-    String fruit = fruits.get(2); // Retrieves the 3rd element from the ArrayList
+    
+    String fruit = fruits.get(2); 
 
-    // Print the retrieved element
+    
     System.out.println("The retrieved element is: " + fruit);
   }
 }
